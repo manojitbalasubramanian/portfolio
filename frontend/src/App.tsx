ffd4102b8c0e153f003a21da451d62c2fed408dd
manojitbalasubramanian <manojitbalasubramanianr@gmail.com>
2025-06-16 11:56:08 +0530

    deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App() {
-  return <div className="App">./app</div>;
+  return (
+    <div className="App">
+      <h1>Vanakam nanbargale🙏</h1>
+      <p>--by manojitbalasubramanian</p>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter basename="/portfolio">
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
